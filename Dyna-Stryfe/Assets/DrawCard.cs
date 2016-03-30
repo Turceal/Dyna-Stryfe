9790656acf1915e52457ed8c6a28ebcb1b763cdf
Turceal <floryew@gmail.com>
2016-03-30 19:16:39 -0400

    Unity code
    uploading unity instead of java

---
+++ Dyna-Stryfe/Assets/DrawCard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawCard : MonoBehaviour {
+
+	public GameObject card= null;
+	// Use this for initialization
+	void Start () {
+		Instantiate (card);
+	}
+	
+
+}
